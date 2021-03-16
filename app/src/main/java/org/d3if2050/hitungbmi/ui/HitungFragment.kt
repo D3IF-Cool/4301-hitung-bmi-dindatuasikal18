e83d0b3ab645d40a33918d49a8e72f07b9fe46ad
dinda durimahu tuasikal <dindatuasikal@student.telkomuniversity.ac.id>
2021-03-17 00:11:27 +0700

    Membuat navigasi menggunakan menu

--- app/src/main/java/org/d3if2050/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if2050/hitungbmi/ui/HitungFragment.kt
@@ -2,12 +2,12 @@ package org.d3if2050.hitungbmi.ui
 
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import org.d3if2050.hitungbmi.R
 import org.d3if2050.hitungbmi.data.KategoriBmi
 import org.d3if2050.hitungbmi.databinding.FragmentHitungBinding
@@ -28,9 +28,22 @@ class HitungFragment : Fragment() {
 
             )
         }
-
+        setHasOptionsMenu(true)
         return binding.root
     }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(
+                R.id.action_hitungFragment_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun hitungBmi() {
         val berat = binding.beratEditText.text.toString()
         if (TextUtils.isEmpty(berat)) {
